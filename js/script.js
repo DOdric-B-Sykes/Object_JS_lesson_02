63ecd132938dbaa8286c616c332a3b393301d347
D_Sykes <Itachi_15_90@yahoo.com>
2024-03-06 21:40:40 -0600

    Updated JS

--- js/script.js
+++ js/script.js
@@ -14,7 +14,20 @@ const learning = {
   category: "Front End Development",
   topicImportance: "high",
   hoursThisWeek: 2,
-  weeklyHourGoal: 7
+  weeklyHourGoal: 7,
+  achievedStudyGoal: false,
+  addStudyTime: function (hours){
+    this.hoursThisWeek = (hours += 2); /* "this." is used to selected specific object property I would like to affect */
+    if (this.hoursThisWeek = (this.hoursThisWeek >= this.weeklyHourGoal)){
+      achievedStudyGoal = true 
+      this.celebrate = console.log(this.celebrate());
+    };
+  },
+  celebrate: function(){
+    body.classList.add("celebrate");
+    success.classList.remove("hide"); /* success class is element being selcected */
+    dance.classList.remove("hide"); /* dance class is the element being selected */
+  }
 };
 
 topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
@@ -22,3 +35,6 @@ countElement.innerText = `✔️ I have ${learning.learningGoals.length} learnin
 
 topicElement.classList.remove("hide");
 countElement.classList.remove("hide");
+
+
+learning.addStudyTime(10);
